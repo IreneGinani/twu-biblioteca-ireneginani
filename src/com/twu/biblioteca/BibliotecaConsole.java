c7ad375f9e249fa8cc4aa1dc47c4112f9b76d8b4
Pedro Queiroz <pedroqueiroz942@gmail.com>
2019-04-04 14:22:36 -0300

    [Irene/Pedro][Story1.3] Make test pass by adding a equals() method to the Book class

--- src/com/twu/biblioteca/BibliotecaConsole.java
+++ src/com/twu/biblioteca/BibliotecaConsole.java
@@ -1,12 +1,18 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.domain.Book;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class BibliotecaConsole {
 
-    public List<String> getBookList() {
-        return Arrays.asList("Harry Potter and The Sorcerer's Stone",  "Homo Deus", "Sapiens");
+    public List<Book> getBookList() {
+        Book harryPotter = new Book("Harry Potter and The Sorcerer's Stone", "J.K Rowling", 1997);
+        Book homoDeus = new Book("Homo Deus", "Harari", 2015);
+        Book sapiens = new Book("Sapiens", "Harari", 2011);
+
+        return Arrays.asList(harryPotter, homoDeus, sapiens);
     }
 
     public String getWelcomeMessage() {
--- src/com/twu/biblioteca/domain/Book.java
+++ src/com/twu/biblioteca/domain/Book.java
@@ -1,7 +1,38 @@
 package com.twu.biblioteca.domain;
 
+import java.util.Objects;
+
 public class Book {
 
     public Book(String title, String author, int year) {
+        this.author = author;
+        this.title = title;
+        this.year = year;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(author, book.author) &&
+                Objects.equals(title, book.title) &&
+                Objects.equals(year, book.year);
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    private String author;
+    private String title;
+    private int year;
 }
