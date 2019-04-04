7b5dbbe97442ef332f386e37929a64f8bddd6d28
Pedro Queiroz <pedroqueiroz942@gmail.com>
2019-04-04 18:22:24 -0300

    [Irene/Pedro][Story1.5] Write test to make sure the book list is printed in the correct format

--- test/com/twu/biblioteca/console/BibliotecaConsoleTest.java
+++ test/com/twu/biblioteca/console/BibliotecaConsoleTest.java
@@ -1,7 +1,11 @@
 package com.twu.biblioteca.console;
 
+import com.twu.biblioteca.domain.Book;
 import com.twu.biblioteca.domain.Library;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -25,5 +29,19 @@ public class BibliotecaConsoleTest {
         String invalidOptionMessage = "Please select a valid option!";
 
         assertEquals(invalidOptionMessage, bibliotecaConsole.processOption(123));
+    }
+
+    @Test
+    public void shouldReturnListOfAllBooksWhenOptionSelectedIsOne() {
+        Library library = new Library();
+        BibliotecaConsole bibliotecaConsole = new BibliotecaConsole(library);
+        String expectedOutput = "Title - Author - Year\n" +
+                                "Harry Potter and The Sorcerer's Stone - J.K Rowling - 1997\n" +
+                                "Homo Deus - Harari - 2015\n" +
+                                "Sapiens - Harari - 2011\n";
+
+        int option = 1;
+
+        assertEquals(expectedOutput, bibliotecaConsole.processOption(option));
     }
 }
