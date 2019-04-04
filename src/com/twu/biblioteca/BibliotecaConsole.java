cf8c4f73ff25cbbdbda96841e52f4beb4f8dd67c
Pedro Queiroz <pedroqueiroz942@gmail.com>
2019-04-04 13:22:52 -0300

    [Irene/Pedro][Story1.2] Add test to return all books

--- src/com/twu/biblioteca/BibliotecaConsole.java
+++ src/com/twu/biblioteca/BibliotecaConsole.java
@@ -1,6 +1,12 @@
 package com.twu.biblioteca;
 
+import java.util.List;
+
 public class BibliotecaConsole {
+
+    public List<String> getBookList() {
+        return null;
+    }
 
     public String getWelcomeMessage() {
         return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
--- test/com/twu/biblioteca/BibliotecaConsoleTest.java
+++ test/com/twu/biblioteca/BibliotecaConsoleTest.java
@@ -1,6 +1,10 @@
 package com.twu.biblioteca;
 
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class BibliotecaConsoleTest {
@@ -12,5 +16,14 @@ public class BibliotecaConsoleTest {
         String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
 
         assertEquals(welcomeMessage, bibliotecaConsole.getWelcomeMessage());
+    }
+
+    @Test
+    public void shouldReturnListOfAllBooks() {
+        BibliotecaConsole bibliotecaConsole = new BibliotecaConsole();
+
+        List<String> bookList = Arrays.asList("Harry Potter and The Sorcerer's Stone",  "Homo Deus", "Sapiens");
+
+        assertEquals(bookList, bibliotecaConsole.getBookList());
     }
 }
