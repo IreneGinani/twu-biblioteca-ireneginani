7048faa04d053542859cf3169df76ceab750373a
Pedro Queiroz <pedroqueiroz942@gmail.com>
2019-04-04 17:59:44 -0300

    [Irene/Pedro][Story1.4] Print book list after selecting an option

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,18 +2,28 @@ package com.twu.biblioteca;
 
 import com.twu.biblioteca.console.BibliotecaConsole;
 import com.twu.biblioteca.domain.Book;
+import com.twu.biblioteca.domain.Library;
+
+import java.util.Scanner;
 
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        BibliotecaConsole bibliotecaConsole = new BibliotecaConsole();
+        Library library = new Library();
+
+        BibliotecaConsole bibliotecaConsole = new BibliotecaConsole(library);
 
         System.out.println(bibliotecaConsole.getWelcomeMessage());
 
-        System.out.println("Title - Author - Year");
+        System.out.println("Choose an option:");
+        System.out.println("(1) List of Books");
 
-        for (Book book: bibliotecaConsole.getBookList()) {
-            System.out.println(book.getTitle() + " - " + book.getAuthor() + " - " + book.getYear());
-        }
+        Scanner scan = new Scanner(System.in);
+
+        int option = scan.nextInt();
+
+        bibliotecaConsole.processOption(option);
+
+
     }
 }
