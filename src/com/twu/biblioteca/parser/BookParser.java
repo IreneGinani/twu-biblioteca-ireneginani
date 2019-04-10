ca425a49a8aa24429dbd3046217b1c079f968433
Irene Ginani <ireneginani@gmail.com>
2019-04-10 10:23:29 -0300

    [Irene] Create test to check book list in a file

---
+++ src/com/twu/biblioteca/parser/BookParser.java
@@ -0,0 +1,11 @@
+package com.twu.biblioteca.parser;
+
+import com.twu.biblioteca.domain.Book;
+
+import java.util.List;
+
+public class BookParser {
+    
+    public List<Book> getBookList() {
+    }
+}
---
+++ test/com/twu/biblioteca/parser/BookParserTest.java
@@ -0,0 +1,28 @@
+package com.twu.biblioteca.parser;
+
+import com.twu.biblioteca.domain.Book;
+import com.twu.biblioteca.domain.Library;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class BookParserTest {
+
+    @Test
+    public void shouldReturnFalseWhenCheckoutSameBookTwice() {
+        BookParser parser = new BookParser();
+
+        Book harryPotter = new Book("Harry Potter and The Sorcerer's Stone", "J.K Rowling", 1997);
+        Book homoDeus = new Book("Homo Deus", "Harari", 2015);
+        Book sapiens = new Book("Sapiens", "Harari", 2011);
+
+        List<Book> books = new ArrayList<>(Arrays.asList(harryPotter, homoDeus, sapiens));
+
+        assertEquals(books, parser.getBookList());
+    }
+    }
+}
