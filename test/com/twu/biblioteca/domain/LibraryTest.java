20bfed9d59dfcad68e6537c9846f0415e7f26f47
Irene Ginani <ireneginani@gmail.com>
2019-04-10 14:52:55 -0300

    [Irene] Change service package name

--- test/com/twu/biblioteca/domain/LibraryTest.java
+++ test/com/twu/biblioteca/domain/LibraryTest.java
@@ -1,6 +1,6 @@
 package com.twu.biblioteca.domain;
 
-import com.twu.biblioteca.Service.LibraryService;
+import com.twu.biblioteca.service.LibraryService;
 import org.junit.Before;
 import org.junit.Test;
 
