80c004442af00f7526c1423bfdee53d8aa154f26
Irene Ginani <ireneginani@gmail.com>
2019-04-13 14:17:03 -0300

    [Irene][Story2.3] create user functionalities

--- src/com/twu/biblioteca/domain/User.java
+++ src/com/twu/biblioteca/domain/User.java
@@ -1,4 +1,20 @@
 package com.twu.biblioteca.domain;
 
 public class User {
+
+    private String libraryLogin;
+    private String name;
+    private String role;
+
+    public User(String libraryLogin, String name, String role) {
+        this.libraryLogin = libraryLogin;
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getLibraryLogin() { return libraryLogin; }
+
+    public String getName() { return name; }
+
+    public String getRole() { return role; }
 }
--- src/com/twu/biblioteca/parser/UserParser.java
+++ src/com/twu/biblioteca/parser/UserParser.java
@@ -1,4 +1,44 @@
 package com.twu.biblioteca.parser;
 
+import com.twu.biblioteca.domain.User;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class UserParser {
+    
+    private List<User> users = new ArrayList<>();
+
+    public UserParser(){
+        UserParser();
+    }
+
+    public List<User> UserParser(){
+        try {
+
+            Scanner userFile = new Scanner(new FileReader("src/com/twu/biblioteca/data/users.txt"));
+
+            while (userFile.hasNextLine()){
+                String line = userFile.nextLine();
+
+                String [] userAttributes = line.split(", ");
+
+                User user = new User(userAttributes[0], userAttributes[1], userAttributes[2]);
+
+                users.add(user);
+            }
+
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("No such file or directory");
+        }
+
+        return users;
+    }
+
+    public List<User> getUserList() {
+        return users;
+    }
 }
